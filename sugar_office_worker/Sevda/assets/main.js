6abba8f19f9fb2fc0488d3aaa1f93b71213ee614
Sevda <morkocsevdam@gmail.com>
2023-12-18 16:00:32 +0300

    edited

--- sugar_office_worker/Sevda/assets/main.js
+++ sugar_office_worker/Sevda/assets/main.js
@@ -10,7 +10,17 @@ function showSlides() {
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.transform = 'translateX(' + (-100 * (slideIndex - 1)) + '%)';
     }
-    setTimeout(showSlides, 2000); // 2000 milisaniye (2 saniye) sonra bir sonraki slayda geç
+    setTimeout(showSlides, 3000); // 2000 milisaniye (3 saniye) sonra bir sonraki slayda geç
 }
 
 showSlides(); // Sayfa yüklendiğinde otomatik başlat
+
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+    }
+    // Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+    }
